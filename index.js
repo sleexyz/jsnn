48418031ce8760487bd5bdf72a1dfcfad2c45191
Sean Lee <freshdried@gmail.com>
2017-05-09 19:20:22 -0400

    wip

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ type Model = {
   bias: number
 };
 
+const setW0 = (w0, model): Model => ({ weights: [ w0, model.weights[1] ], bias: model.bias });
+const setW1 = (w1, model): Model => ({ weights: [ model.weights[0], w1 ], bias: model.bias });
+const setB = (b, model): Model => ({ weights: model.weights, bias: b });
+
+type Data = Array<{ input: Array<number>, expectedOutput: number}>;
+
 const makePerceptron = ({ weights, bias }: Model) => (input: Array<number>): number => {
   let acc = 0;
   for (let i = 0; i < weights.length; i += 1) {
     acc += weights[i] * input[i];
   }
   return 1 / (1 + Math.exp(-1 * (acc + bias)));
-  // if (acc + bias > 0) {
-  //   return 1;
-  // } else {
-  //   return 0;
-  // }
 };
 
 const generatePerceptronModel = () => {
@@ -25,79 +26,112 @@ const generatePerceptronModel = () => {
   return { weights, bias };
 };
 
-
 const numericalDerivative = (f: number => number) => (x0: number): number => {
   const delta = 0.0001;
   const nd = (f(x0 + delta) - f(x0)) / delta
   return nd
 };
 
-const setW0 = (w0, model): Model => ({
-  weights: [
-    w0,
-    model.weights[1],
-  ],
-  bias: model.bias,
-});
 
-const setW1 = (w1, model): Model => ({
-  weights: [
-    model.weights[0],
-    w1,
-  ],
-  bias: model.bias,
-});
-
-const setB = (b, model): Model => ({
-  weights: model.weights,
-  bias: b,
-});
-
-const mse = (a, b) => Math.pow((a - b), 2) / 2;
-
-const getCost = (input: Array<number>, expectedOutput: number) => (model: Model): number => {
-  const output = makePerceptron(model)(input);
-  return mse(output, expectedOutput);
-}
-
-
-const step = (input: Array<number>, expectedOutput: number) => (model: Model): { cost: number, nextModel: Model }=> {
-  const _getCost = getCost(input, expectedOutput);
-  const cost = _getCost(model); // FIXME: optimize later
-
-  const dw0 = numericalDerivative((w0) => _getCost(setW0(w0, model)))(model.weights[0]);
-  const dw1 = numericalDerivative((w1) => _getCost(setW1(w1, model)))(model.weights[1]);
-  const db = numericalDerivative((b) => _getCost(setB(b, model)))(model.bias);
-
-  const learningRate = 1;
-
-  const nextModel = {
-    weights: [
-      model.weights[0] - (dw0 * learningRate),
-      model.weights[1] - (dw1 * learningRate),
-    ],
-    bias: model.bias - (db * learningRate),
+// SGD with batch size of 1
+const train0: Data => Model => Model = (() => {
+  const getCost = (input: Array<number>, expectedOutput: number) => (model: Model): number => {
+    const output = makePerceptron(model)(input);
+    return Math.pow(output - expectedOutput, 2); // squared error
   };
 
-  console.log(cost)
-  return {
-    cost,
-    nextModel,
-  };
-};
+  const step = (input: Array<number>, expectedOutput: number) => (model: Model): { cost: number, nextModel: Model } => {
+    const _getCost = getCost(input, expectedOutput);
+    const cost = _getCost(model); // FIXME: optimize later
 
-const train = (data: Array<{ input: Array<number>, expectedOutput: number }>) => (model: Model): Model=> {
-  let m = model;
-  for (let i = 0; i < data.length; i += 1 ) {
-    const { input, expectedOutput } = data[i];
-    const { cost, nextModel } = step(input, expectedOutput)(m);
-    m = nextModel;
-  }
-  return m
-}
+    const dw0 = numericalDerivative((w0) => _getCost(setW0(w0, model)))(model.weights[0]);
+    const dw1 = numericalDerivative((w1) => _getCost(setW1(w1, model)))(model.weights[1]);
+    const db = numericalDerivative((b) => _getCost(setB(b, model)))(model.bias);
+
+    const learningRate = 0.1;
+
+    const nextModel = {
+      weights: [
+        model.weights[0] - (dw0 * learningRate),
+        model.weights[1] - (dw1 * learningRate),
+      ],
+      bias: model.bias - (db * learningRate),
+    };
+
+    return {
+      cost,
+      nextModel,
+    };
+  };
+
+  const train = (data: Data) => (model: Model): Model=> {
+    let m = model;
+    for (let i = 0; i < data.length; i += 1 ) {
+      const { input, expectedOutput } = data[i];
+      const { cost, nextModel } = step(input, expectedOutput)(m);
+      m = nextModel;
+    }
+    return m
+  };
+
+  return train;
+})();
+
+// GD with batch size of 1
+const train1: { steps: number } => Data => Model => Model = (() => {
+
+  // mse
+  const getCost = (data) => (model: Model): number => {
+    let acc = 0;
+    for (let i = 0; i < data.length; i += 1) {
+      const { input, expectedOutput } = data[i];
+      const output = makePerceptron(model)(input);
+
+      acc += Math.pow(output - expectedOutput, 2); // squared error
+    }
+    return acc / data.length;
+  };
+
+  const step = (data: Data) => (model: Model): { cost: number, nextModel: Model } => {
+    const _getCost = getCost(data);
+    const cost = _getCost(model); // FIXME: optimize later
+
+    const dw0 = numericalDerivative((w0) => _getCost(setW0(w0, model)))(model.weights[0]);
+    const dw1 = numericalDerivative((w1) => _getCost(setW1(w1, model)))(model.weights[1]);
+    const db = numericalDerivative((b) => _getCost(setB(b, model)))(model.bias);
+
+    const learningRate = 0.1;
+
+    const nextModel = {
+      weights: [
+        model.weights[0] - (dw0 * learningRate),
+        model.weights[1] - (dw1 * learningRate),
+      ],
+      bias: model.bias - (db * learningRate),
+    };
+
+    return {
+      cost,
+      nextModel,
+    };
+  };
+
+  const train = (parameters: { steps: number }) => (data: Data) => (model: Model): Model=> {
+    const { steps } = parameters;
+    let m = model;
+    for (let i = 0; i < steps; i += 1 ) {
+      const { cost, nextModel } = step(data)(m);
+      m = nextModel;
+    }
+    return m
+  };
+
+  return train;
+})();
 
 module.exports = {
   makePerceptron,
   generatePerceptronModel,
-  train,
+  train0,
+  train1,
 };
--- test.js
+++ test.js
@@ -1,36 +1,53 @@
-const { makePerceptron, generatePerceptronModel, train } = require('./index.js');
+// @flow
+const {
+  makePerceptron,
+  generatePerceptronModel,
+  train0,
+  train1,
+} = require('./index.js');
 const { expect } = require('chai');
 
 const idealPerceptron = makePerceptron({ weights: [-2, -2], bias: 3 });
 
+const testNand = (perceptron) => {
+  const margin = 0.1;
+  expect(perceptron([1, 0])).to.be.closeTo(0.7310585786300049, margin);
+  expect(perceptron([0, 1])).to.be.closeTo(0.7310585786300049, margin);
+  expect(perceptron([0, 0])).to.be.closeTo(0.9525741268224331, margin);
+  expect(perceptron([1, 1])).to.be.closeTo(0.2689414213699951, margin);
+};
+
 describe("makePerceptron", function () {
   describe("and", function () {
     it("works", function () {
-      expect(idealPerceptron([1, 0])).to.eql(0.7310585786300049);
-      expect(idealPerceptron([0, 1])).to.eql(0.7310585786300049);
-      expect(idealPerceptron([0, 0])).to.eql(0.9525741268224331)
-      expect(idealPerceptron([1, 1])).to.eql(0.2689414213699951)
+      testNand(idealPerceptron);
     });
   });
 });
 
-describe("train", function () {
-  describe("nand", function () {
+describe("training methods", function () {
+  const data = Array(10000).fill('').map((_, i) => {
+    const randomBound = () => 1 - (Math.random() * 2)
+    const randomInput = [ randomBound(), randomBound() ];
+    return {
+      input: randomInput,
+      expectedOutput: idealPerceptron(randomInput),
+    }
+  });
+
+  describe("train0 - SGD with step size 1", function () {
     it("works", function () {
-      const data = Array(1000).fill('').map((_, i) => {
-        const randomBound = () => 1 - (Math.random() * 2)
-        const randomInput = [ randomBound(), randomBound() ];
-        return {
-          input: randomInput,
-          expectedOutput: idealPerceptron(randomInput),
-        }
-      });
-      const model = train(data)(generatePerceptronModel());
-      const perceptron = makePerceptron(model);
-      expect(perceptron([1, 0])).to.be.closeTo(0.7310585786300049, 0.25);
-      expect(perceptron([0, 1])).to.be.closeTo(0.7310585786300049, 0.25);
-      expect(perceptron([0, 0])).to.be.closeTo(0.9525741268224331, 0.25);
-      expect(perceptron([1, 1])).to.be.closeTo(0.2689414213699951, 0.25);
+      const model = train0(data)(generatePerceptronModel());
+      testNand(makePerceptron(model));
+    });
+  });
+
+  describe("train1 - GD", function () {
+    it("works", function () {
+      const model = train1({
+        steps: 1000,
+      })(data)(generatePerceptronModel());
+      testNand(makePerceptron(model));
     });
   });
 });
